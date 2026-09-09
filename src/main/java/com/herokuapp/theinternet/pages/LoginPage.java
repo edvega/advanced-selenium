23db574b25864c2b204d390617d12291051eb015
Edwin V. <edwega@gmail.com>
2026-09-09 13:56:38 -0600

    refactored NegativeLoginTests

--- src/main/java/com/herokuapp/theinternet/pages/LoginPage.java
+++ src/main/java/com/herokuapp/theinternet/pages/LoginPage.java
@@ -4,11 +4,14 @@ import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import java.time.Duration;
+
 public class LoginPage extends BasePage {
 
     private final By usernameLocator = By.id("username");
     private final By passwordLocator = By.name("password");
     private final By loginButtonLocator = By.tagName("button");
+    private final By errorMessageLocator = By.id("flash");
 
     public LoginPage(WebDriver driver, Logger log) {
         super(driver, log);
@@ -21,5 +24,21 @@ public class LoginPage extends BasePage {
         type(password, passwordLocator);
         click(loginButtonLocator);
         return new SecureAreaPage(driver, log);
+    }
+
+    /** Execute negative login */
+    public void negativelogIn(String username, String password) {
+        log.info("Executing negative login with username [{}] and password [{}]", username, password);
+        type(username, usernameLocator);
+        type(password, passwordLocator);
+        click(loginButtonLocator);
+    }
+
+    public void waitForErrorMessage() {
+        waitForVisibilityOf(errorMessageLocator, Duration.ofSeconds(5));
+    }
+
+    public String getErrorMessageText() {
+        return find(errorMessageLocator).getText();
     }
 }
--- src/test/java/com/herokuapp/theinternet/loginpagetest/NegativeLoginTests.java
+++ src/test/java/com/herokuapp/theinternet/loginpagetest/NegativeLoginTests.java
@@ -1,7 +1,8 @@
 package com.herokuapp.theinternet.loginpagetest;
 
 import com.herokuapp.theinternet.base.TestUtilities;
-import org.openqa.selenium.By;
+import com.herokuapp.theinternet.pages.LoginPage;
+import com.herokuapp.theinternet.pages.WelcomePage;
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
@@ -12,26 +13,11 @@ public class NegativeLoginTests extends TestUtilities {
     @Test(priority = 1)
     public void negativeTest(String username, String password, String expectedErrorMessage) {
         log.info("Starting negativeTest");
-
-        // open main page
-        String url = "http://the-internet.herokuapp.com/";
-        driver.get(url);
-        log.info("Main page is opened.");
-
-        // Click on Form Authentication link
-        driver.findElement(By.linkText("Form Authentication")).click();
-
-        // enter username and password
-        driver.findElement(By.id("username")).sendKeys(username);
-        driver.findElement(By.id("password")).sendKeys(password);
-
-        // push log in button
-        driver.findElement(By.className("radius")).click();
-
-        // Verification
-        String actualErrorMessage = driver.findElement(By.id("flash")).getText();
-        Assert.assertTrue(actualErrorMessage.contains(expectedErrorMessage),
-                "actualErrorMessage does not contain expectedErrorMessage\nexpectedErrorMessage: "
-                        + expectedErrorMessage + "\nactualErrorMessage: " + actualErrorMessage);
+        WelcomePage welcomePage = new WelcomePage(driver, log);
+        welcomePage.openPage();
+        LoginPage loginPage = welcomePage.clickFormAuthenticationLink();
+        loginPage.negativelogIn(username, password);
+        loginPage.waitForErrorMessage();
+        Assert.assertTrue(loginPage.getErrorMessageText().contains(expectedErrorMessage));
     }
 }
